30ef00ee30fadd2c24380c88d8113f986a7c83df
Majdi REKIK <86914108+SiSmog@users.noreply.github.com>
2023-11-14 23:45:18 +0100

    Delete Backend/main.js

--- Backend/main.js
+++
@@ -1,5 +0,0 @@
-import {save,pay} from "./interact.js"
-import email from './models/email.js'
-email("majdirekik60@gmail.com",`google.com`)
-//pay("0x64C25e2C6035c3B7d8BC93638e827F43d85fedbb",10);
-//pay("0x64C25e2C6035c3B7d8BC93638e827F43d85fedbb",10);
